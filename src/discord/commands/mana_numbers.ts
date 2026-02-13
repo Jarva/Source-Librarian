271d0f1f5e40ac09aec28422685bdb2160ddf8c3
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2026-02-13 22:40:20 +0100

    Update mana_numbers.ts

--- src/discord/commands/mana_numbers.ts
+++ src/discord/commands/mana_numbers.ts
@@ -3,7 +3,7 @@ import {BaseEmbedCommand} from "@/discord/abstracts/embed-command.ts";
 const lines = [
     "Back in 1.16.5, the NumericHUD is a feature bundled in the Ars Ender Storage addon (requires the Ender Storage mod too).",
     "From 1.18 up to 1.20.1, the Numeric HUD is provided by Ars Instrumentum wearing the Numeric Charm, a client config can be used to force the hud on even while not wearing it.",
-    "In 1.20.1 there's also Ars Mana Meter addon, which toggle the debug number flag on while also allowing some repositioning pf the bar.",
+    "In 1.20.1 there's also Ars Mana Meter addon which in addition to enabling the debug numbers also allows some adjustments for the bar position.",
     "In 1.21 there are multiple ways:",
     "- Use the `/ars debug on` command, requires admin perms, turns them on for the current session. Can also be used to turn the other methods off temporarily.",
     "- add Ars Numeric HUD, only thing it does is turning the switch on.",
