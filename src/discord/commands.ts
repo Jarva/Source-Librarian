de6599b960e16437fa06b2a32cc956c2f7dc6fa8
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2026-02-13 23:48:53 +0100

    Add Mana Numbers info command (#6)

--- src/discord/commands.ts
+++ src/discord/commands.ts
@@ -1,32 +1,34 @@
-import { IframesCommand } from "@/discord/commands/iframes.ts";
-import { Command } from "@buape/carbon";
-import { ClaimsCommand } from "@/discord/commands/claims.ts";
-import { ImbuementCommand } from "@/discord/commands/imbuement.ts";
-import { ManaCommand } from "@/discord/commands/mana.ts";
-import { LogsCommand } from "@/discord/commands/logs.ts";
-import { EnchantingCommand } from "@/discord/commands/enchanting.ts";
-import { AddonsCommand } from "@/discord/commands/addons/command.ts";
-import { LagCommand } from "@/discord/commands/lag.ts";
-import { InfiniteGlyphsCommand } from "@/discord/commands/infinite-glyphs.ts";
-import { PinInThreadCommand } from "@/discord/commands/pin-in-thread.ts";
-import { LmgtfyCommand } from "@/discord/commands/lmgtfy.ts";
-import { BinarySearchCommand } from "@/discord/commands/binary-search.ts";
-import { AncientCommand } from "@/discord/commands/ancient.ts";
-import { StarbuncleAdoptionCommand } from "@/discord/commands/starbuncle-adoption.ts";
+import {IframesCommand} from "@/discord/commands/iframes.ts";
+import {Command} from "@buape/carbon";
+import {ClaimsCommand} from "@/discord/commands/claims.ts";
+import {ImbuementCommand} from "@/discord/commands/imbuement.ts";
+import {ManaCommand} from "@/discord/commands/mana.ts";
+import {LogsCommand} from "@/discord/commands/logs.ts";
+import {EnchantingCommand} from "@/discord/commands/enchanting.ts";
+import {AddonsCommand} from "@/discord/commands/addons/command.ts";
+import {LagCommand} from "@/discord/commands/lag.ts";
+import {InfiniteGlyphsCommand} from "@/discord/commands/infinite-glyphs.ts";
+import {PinInThreadCommand} from "@/discord/commands/pin-in-thread.ts";
+import {LmgtfyCommand} from "@/discord/commands/lmgtfy.ts";
+import {BinarySearchCommand} from "@/discord/commands/binary-search.ts";
+import {AncientCommand} from "@/discord/commands/ancient.ts";
+import {StarbuncleAdoptionCommand} from "@/discord/commands/starbuncle-adoption.ts";
+import {NumbersCommand} from "./commands/mana_numbers.ts";
 
 export default [
-  new IframesCommand(),
-  new ClaimsCommand(),
-  new ImbuementCommand(),
-  new EnchantingCommand(),
-  new LogsCommand(),
-  new ManaCommand(),
-  new AddonsCommand(),
-  new LagCommand(),
-  new InfiniteGlyphsCommand(),
-  new PinInThreadCommand(),
-  new LmgtfyCommand(),
-  new BinarySearchCommand(),
-  new AncientCommand(),
-  new StarbuncleAdoptionCommand(),
+    new IframesCommand(),
+    new ClaimsCommand(),
+    new ImbuementCommand(),
+    new EnchantingCommand(),
+    new LogsCommand(),
+    new ManaCommand(),
+    new AddonsCommand(),
+    new LagCommand(),
+    new InfiniteGlyphsCommand(),
+    new PinInThreadCommand(),
+    new LmgtfyCommand(),
+    new BinarySearchCommand(),
+    new AncientCommand(),
+    new StarbuncleAdoptionCommand(),
+    new NumbersCommand()
 ] as Command[];
